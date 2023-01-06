8ff3cb052abd37bb187efa2be5647b4f97295224
wowsnickerswow <your_github_email>
2023-01-06 21:27:31 +0100

    new commit space

--- components/ItemContainer.jsx
+++ components/ItemContainer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ItemContainer = () => (
   <div className="my-8">
     <div className="flex flex-col items-stretch md:flex-row md:max-w-5xl rounded-lg bg-white shadow-lg ">
-      <div className="p-6 flex flex-col justify-between w-full"/>
+      <div className="p-6 flex flex-col justify-between w-full" />
     </div>
   </div>
 );
